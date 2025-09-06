e2140aab82890132a52014ed5ecd76a4ab98d41c
Tony <talperov10@gmail.com>
2025-09-06 00:16:37 -0700

    Update dmm.c

--- Alperov_PA2/dmm.c
+++ Alperov_PA2/dmm.c
@@ -10,3 +10,64 @@ requirments.
 */
 #include "dmm.h"
 
+void main_menu(void)
+{
+	printf("              ---------------------------------Welcome to Digital Music Manager---------------------------------\n");
+	// Will eventually get user input from to execute command.
+}
+
+Node* makeNode(Record newData)
+{
+	Node* pMem = (Node*)malloc(sizeof(Node));
+
+	if (pMem != NULL)
+	{
+		pMem->data = newData;
+		pMem->next = NULL;
+		pMem->prev = NULL;
+	}
+	return pMem;
+}
+
+int insertFront(Node** pHead, Record newData)
+{
+	Node* pMem = makeNode(newData);
+	if (pMem == NULL)
+	{
+		return 0;
+	}
+
+	pMem->next = *pHead;
+	pMem->prev = NULL;
+	
+	if (pMem != NULL)
+	{
+		(*pHead)->prev = pMem;
+	}
+	*pHead = pMem;
+
+	return 1;
+}
+
+void printList(Node* pHead)
+{
+	Node* pointer = pHead;
+	if (pointer == NULL)
+	{
+		printf("List is empty.\n");
+		return;
+	}
+	while (pointer != NULL)
+	{
+		printf("Artist: %s\n", pointer->data.artist);
+		printf("Album: %s\n", pointer->data.album_title);
+		printf("Song: %s\n", pointer->data.song_title);
+		printf("Genre: %s\n", pointer->data.genre);
+		printf("Times Played: %d\n\n", pointer->data.times_played);
+		printf("Duration:  %d:%02d\n", pointer->data.song_length.minutes, pointer->data.song_length.seconds);
+		printf("Rating: %d\n", pointer->data.rating);
+
+		pointer = pointer->next;
+
+	}
+}
